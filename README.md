f6139bb8a6e495ad3735cc1c809873542c43d79d
LeoDog896 <LeoDog896@hotmail.com>
2022-09-07 17:17:11 +0000

    IP

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 port ducts. UDP and TCP.
 
+`puct tcp 381.281.301.301` - establishes a UTP (TCP over UDP) connection
+
+`puct tcp 692.412.825.504 25565` - establishes a TCP connection with tcp/25565 being the input
+
+You can also specify ports, but 18479 will be the default:
+
+`puct udp 481.351.485.245:5713`
+
+The other party needs to use your IP (which you can get using `puct ip`)
+
 ## quick nat hole punching
 
 Assume client A and B are behind NATs, and they know eachother's public IP addresses and ports.
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,60 @@
+use anyhow::Result;
 use clap::Parser;
 use tokio::net::UdpSocket;
-use anyhow::Result;
 
-/// PUCT
+/// auto-reconnecting p2p streaming for UDP / UDP + TCP (UTP)
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Hosting Address
-   #[clap(value_parser)]
-   source_address: String,
+    #[clap(subcommand)]
+    action: Action,
+}
 
-    /// Destination Address
-    #[clap(value_parser)]
-    destination_address: String
+#[derive(clap::Subcommand, Debug)]
+enum Action {
+    IP,
+    Udp {
+        /// Hosting Address
+        #[clap(value_parser)]
+        source_address: String,
+
+        /// Destination Address
+        #[clap(value_parser)]
+        destination_address: String,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let args = Args::parse();
 
-    let sock = UdpSocket::bind(args.source_address).await?;
+    match args.action {
+        Action::IP => {
+            println!(
+                "{}",
+                public_ip::addr_v4()
+                    .await
+                    .map(|ip| ip.to_string())
+                    .unwrap_or_else(|| "Ip address not found".to_string())
+            );
+        }
+        Action::Udp {
+            source_address,
+            destination_address,
+        } => {
+            let sock = UdpSocket::bind(source_address).await?;
 
-    sock.connect(args.destination_address).await?;
+            sock.connect(destination_address).await?;
 
-    let mut buf = [0; 1024];
-    loop {
-        let len = sock.recv(&mut buf).await?;
-        println!("{:?} bytes received", len);
+            let mut buf = [0; 1024];
+            loop {
+                let len = sock.recv(&mut buf).await?;
+                println!("{:?} bytes received", len);
 
-        let len = sock.send(&buf[..len]).await?;
-        println!("{:?} bytes sent", len);
+                let len = sock.send(&buf[..len]).await?;
+                println!("{:?} bytes sent", len);
+            }
+        }
     }
+    Ok(())
 }
