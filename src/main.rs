72d6a7e589d520e6e1c08f300f45e09f228d108b
LeoDog896 <LeoDog896@hotmail.com>
2022-09-08 12:11:11 +0000

    Cleanup, cloning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::net::{SocketAddrV4, Ipv4Addr};
 use anyhow::Result;
 use clap::Parser;
-use tokio::{io::{self, BufReader, AsyncBufReadExt}, net::UdpSocket};
+use std::net::{Ipv4Addr, SocketAddrV4};
+use tokio::net::UdpSocket;
 
 /// Auto-reconnecting p2p streaming for UDP / UDP + TCP (UTP)
 #[derive(Parser, Debug)]
@@ -17,7 +17,6 @@ enum Action {
     IP,
     /// Runs puct with the UDP protocol
     Udp {
-
         /// Destination Address
         #[clap(value_parser)]
         destination_address: String,
@@ -38,18 +37,30 @@ async fn main() -> Result<()> {
                 "{}",
                 public_ip::addr_v4()
                     .await
-                    .map(|ip| ip.to_string())
-                    .unwrap_or_else(|| "Ip address not found".to_string())
+                    .map_or_else(|| "Ip address not found".to_string(), |ip| ip.to_string())
             );
         }
         Action::Udp {
             source_address,
             destination_address,
         } => {
-            let public_addr = public_ip::addr_v4().await.map(|addr| SocketAddrV4::new(addr, 5713));
-            let parsed_address = source_address.map(|str| {
-                str.parse::<SocketAddrV4>().or_else(|| source_address.parse::<Ipv4Addr>().map(|addr| SocketAddrV4::new(addr, 5713)))
-            }).transpose()?.or_else(|| public_addr).expect("Could not find a public IP address");
+            let public_addr = public_ip::addr_v4()
+                .await
+                .map(|addr| SocketAddrV4::new(addr, 5713));
+            let parsed_address = source_address
+                .clone()
+                .map(|str| {
+                    str.parse::<SocketAddrV4>().or_else(|_| {
+                        source_address
+                            .clone()
+                            .unwrap()
+                            .parse::<Ipv4Addr>()
+                            .map(|addr| SocketAddrV4::new(addr, 5713))
+                    })
+                })
+                .transpose()?
+                .or(public_addr)
+                .expect("Could not find a public IP address");
             let sock = UdpSocket::bind(parsed_address).await?;
 
             sock.connect(destination_address).await?;
@@ -58,14 +69,6 @@ async fn main() -> Result<()> {
             loop {
                 let len = sock.recv(&mut buf).await?;
                 println!("{:?} bytes received", len);
-
-                tokio::spawn(async move {
-                    let stdin = io::stdin();
-                    let reader = BufReader::new(stdin);
-                    let mut lines = reader.lines();
-
-                    lines.next_line().await
-                });
 
                 let len = sock.send(&buf[..len]).await?;
                 println!("{:?} bytes sent", len);
